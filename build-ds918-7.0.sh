a615859d63c8efac6203a449c54871179f250947
qcs1 <95894035+qcs1@users.noreply.github.com>
2021-12-10 14:44:06 +0800

    Update build-ds918-7.0.sh

--- build-ds918-7.0.sh
+++ build-ds918-7.0.sh
@@ -17,7 +17,8 @@ mkdir apollolake
 tar -C./apollolake/ -xf ds.apollolake-7.0.dev.txz usr/local/x86_64-pc-linux-gnu/x86_64-pc-linux-gnu/sys-root/usr/lib/modules/DSM-7.0/build
 
 # 编译 redpill-lkm
-cd redpill-lkm
+cd dsm
+cd redpill-tool-chain
 make LINUX_SRC=../apollolake/usr/local/x86_64-pc-linux-gnu/x86_64-pc-linux-gnu/sys-root/usr/lib/modules/DSM-7.0/build
 read -a KVERS <<< "$(modinfo --field=vermagic redpill.ko)" && cp -fv redpill.ko ../redpill-load/ext/rp-lkm/redpill-linux-v${KVERS[0]}.ko || exit 1
 cd ..
